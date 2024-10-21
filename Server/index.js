5f55b9a89d40c74ae806a2ddfe6336f760d229fe
Nikhil Narra <nikhilnarra@vt.edu>
2024-10-21 18:40:44 -0400

    Updates from 10/21

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 3600;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
 app.listen(PORT, () => {
   console.log("Server up and running at http://localhost:3600");
